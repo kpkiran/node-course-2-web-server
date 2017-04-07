92b174dd55333eb392aa97b6cec8441f534ec452
Ravikiran Padki <Padki.Ravikiran@abc.net.au>
2017-04-07 11:01:51 +1000

    Added a projects page

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Welcome to the creative project space'
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
